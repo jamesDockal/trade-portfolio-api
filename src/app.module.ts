37560f9896ea5db1b70cad219a053b2b2d294845
J Dockal <jamesdockal@gmail.com>
2025-04-08 12:55:10 -0300

    feat: add the flow of portfolio

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PortfolioModule } from './portfolio/portfolio.module';
+import { Portfolio } from './portfolio/entities/portfolio.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST', 'postgres'),
+        port: parseInt(config.get('DB_PORT', '5432')),
+        username: config.get('DB_USERNAME', 'admin'),
+        password: config.get('DB_PASSWORD', '123'),
+        database: config.get('DB_NAME', 'main'),
+        entities: [Portfolio],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    PortfolioModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/portfolio/portfolio.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreatePortfolioDto } from './dto/create-portfolio.dto';
+import { UpdatePortfolioDto } from './dto/update-portfolio.dto';
+import { Portfolio } from './entities/portfolio.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class PortfolioService {
+  constructor(
+    @InjectRepository(Portfolio)
+    private portfolioRepository: Repository<Portfolio>,
+  ) {}
+
+  create(dto: CreatePortfolioDto): Promise<Portfolio> {
+    const portfolio = this.portfolioRepository.create(dto);
+    return this.portfolioRepository.save(portfolio);
+  }
+
+  async findAll(): Promise<Portfolio[]> {
+    return this.portfolioRepository.find();
+  }
+
+  async findOne(id: number): Promise<Portfolio> {
+    const portfolio = await this.portfolioRepository.findOne({ where: { id } });
+    if (!portfolio) {
+      throw new NotFoundException(`Portfolio with ID ${id} not found`);
+    }
+    return portfolio;
+  }
+
+  async update(
+    id: number,
+    updatePortfolioDto: UpdatePortfolioDto,
+  ): Promise<Portfolio> {
+    const portfolio = await this.findOne(id);
+    const updated = Object.assign(portfolio, updatePortfolioDto);
+    return this.portfolioRepository.save(updated);
+  }
+
+  async remove(id: number): Promise<void> {
+    const result = await this.portfolioRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Portfolio with ID ${id} not found`);
+    }
+  }
+}
